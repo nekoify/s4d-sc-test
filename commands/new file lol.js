68672ea4734d76eabeb3416e5e23ae1602941ea4
nekoify <xl83dev@gmail.com>
2023-05-11 11:16:57 +1000

    Updated via S4D

---
+++ commands/new file lol.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+            
+            module.exports = {
+                name: new file lol,
+                run: async (s4d, message) => {
+                    
+                }
+            }
